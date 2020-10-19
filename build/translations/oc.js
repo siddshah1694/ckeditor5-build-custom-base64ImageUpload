6a4b4c75b82f3e8f4dd0cc9903df56dacdbfc6fe
siddshah1694 <36239874+siddshah1694@users.noreply.github.com>
2020-10-19 19:19:56 +0530

    ck editor custom build code

---
+++ build/translations/oc.js
@@ -0,0 +1,1 @@
+(function(n){const r=n["oc"]=n["oc"]||{};r.dictionary=Object.assign(r.dictionary||{},{Bold:"Gras",Cancel:"Anullar",Italic:"Italica","Remove color":"",Save:"Enregistrar",Strikethrough:"",Subscript:"",Underline:""});r.getPluralForm=function(n){return n>1}})(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
